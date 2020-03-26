4c550bad7058866bfdaf193c577a6982845189b3
kajolmehta <kmehta@catalystone.com>
2020-03-26 17:55:04 +0530

    test commit

---
+++ src/main/java/com/compensationmanager/modulecompensationmanager/ModuleCompensationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.compensationmanager.modulecompensationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleCompensationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleCompensationManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compensationmanager/modulecompensationmanager/ModuleCompensationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compensationmanager.modulecompensationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleCompensationManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
